9d84e00f264182852794687d6e68d17907592f9d
Adil Zeshan <adil@zeshan.org>
2019-02-21 00:34:16 +0000

    Add and position each svg stripe

--- src/scripts/presentData.js
+++ src/scripts/presentData.js
@@ -3,11 +3,20 @@ import * as d3 from "d3";
 function presentData(data, id) {
   console.log(id, data);
   const stripeHeight = 400;
+  const stripeWidth = 100 / data.length;
 
   d3.select(id)
     .append("svg")
     .attr("height", stripeHeight)
-    .attr("width", "100%");
+    .attr("width", "100%")
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("height", stripeHeight)
+    .attr("width", stripeWidth + "%")
+    .attr("y", 0)
+    .attr("x", (data, index) => index * stripeWidth + "%");
 }
 
 export default presentData;
