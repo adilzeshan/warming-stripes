0a75c49a7a83c0146c09b8518667f480e25954b0
Adil Zeshan <adil@zeshan.org>
2019-02-21 03:19:37 +0000

    Fix #1. Add colours and ensure they are colourblind-safe

---
+++ src/scripts/colours.js
@@ -0,0 +1,24 @@
+// The following are colourblind-safe, courtesy of http://colorbrewer2.org
+
+const colours = [
+  "#023858",
+  "#045a8d",
+  "#0570b0",
+  "#3690c0",
+  "#74a9cf",
+  "#a6bddb",
+  "#d0d1e6",
+  "#ece7f2",
+  "#fff7fb",
+  "#ffffe5",
+  "#fff7bc",
+  "#fee391",
+  "#fec44f",
+  "#fe9929",
+  "#ec7014",
+  "#cc4c02",
+  "#993404",
+  "#662506"
+];
+
+export default colours;
